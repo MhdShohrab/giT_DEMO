eab952ce6f0252f16c4f54303b097842f1cd6a9d
shohrab <shah@gmail.com>
2024-06-03 12:29:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProfileSection from './ProfileSection';
+import './App.css'; // If you have any global styles
+
+function App() {
+  return (
+    <div className="App">
+      <ProfileSection />
+    </div>
+  );
+}
+
+export default App;
